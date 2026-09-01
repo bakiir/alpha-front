25574456b0b0324176aea7307792e94384b031e1
bakiir <bkiranarbek@gmail.com>
2026-09-01 16:09:56 +0600

    fix: bug in createSubs

--- composables/useSubscriptions.ts
+++ composables/useSubscriptions.ts
@@ -20,6 +20,16 @@ export const useSubscriptions = () => {
     )
   }
 
+  const rescheduleExchange = async (subscriptionId: number, scheduledDate: string) => {
+    return await request<{ message: string; subscription: any }>(
+      `/subscriptions/${subscriptionId}/reschedule-exchange`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ scheduled_date: scheduledDate }),
+      },
+    )
+  }
+
   const fetchNextSet = async (subscriptionId: number) => {
     return await request<{ data: any }>(`/subscriptions/${subscriptionId}/next-set`)
   }
@@ -31,10 +41,42 @@ export const useSubscriptions = () => {
     })
   }
 
+  const createSubscription = async (payload: any) => {
+    return await request<any>('/subscriptions', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+    })
+  }
+
+  const paySubscription = async (subscriptionId: number, paymentMethod: string) => {
+    return await request<any>(`/subscriptions/${subscriptionId}/pay`, {
+      method: 'POST',
+      body: JSON.stringify({ payment_method: paymentMethod }),
+    })
+  }
+
+  const changePlan = async (subscriptionId: number, planId: number) => {
+    return await request<any>(`/subscriptions/${subscriptionId}/change-plan`, {
+      method: 'POST',
+      body: JSON.stringify({ subscription_plan_id: planId }),
+    })
+  }
+
+  const cancelSubscription = async (subscriptionId: number) => {
+    return await request<any>(`/subscriptions/${subscriptionId}/cancel`, {
+      method: 'POST',
+    })
+  }
+
   return {
     fetchMySubscriptions,
     requestExchange,
+    rescheduleExchange,
     fetchNextSet,
     modifySetToys,
+    createSubscription,
+    paySubscription,
+    changePlan,
+    cancelSubscription,
   }
 }
